ce3a48e96d51d5398d23c8f05647f5b00c1a9fd8
Kevin Andel Josy <kevinandeljosy11@gmail.com>
2024-12-16 16:44:41 +0530

    concatenation done

---
+++ Strings/Concatenation.java
@@ -0,0 +1,16 @@
+package Strings;
+
+import java.util.*;
+
+public class Concatenation {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter first name: ");
+    String firstname = sc.nextLine();
+    System.out.println("Enter last name: ");
+    String lastname = sc.nextLine();
+    String fullname = firstname + " " + lastname;
+    System.out.println("Full name is: " + fullname);
+    sc.close();
+  }
+}
