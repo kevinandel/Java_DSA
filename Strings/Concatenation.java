28e1e1c3ef7e2b2084164a20169a7d2416c71ae4
Kevin Andel Josy <kevinandeljosy11@gmail.com>
2024-12-16 17:31:49 +0530

    substring method done

--- Strings/Concatenation.java
+++ Strings/Concatenation.java
@@ -16,15 +16,14 @@ public class Concatenation {
     for (int i = 0; i < fullname.length(); i++) {
       System.out.println(fullname.charAt(i));
     }
-    if(firstname.compareTo(lastname) > 0) {
+    if (firstname.compareTo(lastname) > 0) {
       System.out.println("Firstname is longer");
-    }
-    else if(firstname.compareTo(lastname) == 0) {
+    } else if (firstname.compareTo(lastname) == 0) {
       System.out.println("Firstname and lastname has equal length");
-    }
-    else{
+    } else {
       System.out.println("Last name is longer");
     }
+    System.out.println("Substring of fullname: " + fullname.substring(2, 5));
     sc.close();
   }
 }
